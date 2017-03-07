7c27f6491f43a115e2b3b3633f550d2257d78733
dwb201 <dwb201@hotmail.com>
2017-03-07 09:13:36 +0900

    change

--- weilai-commons/src/main/java/com/weilai/commons/springsecurity/WebSecurityConfig.java
+++ weilai-commons/src/main/java/com/weilai/commons/springsecurity/WebSecurityConfig.java
@@ -1,14 +1,22 @@
 package com.weilai.commons.springsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.*;
 
+import com.weilai.commons.constant.ConstantInfo;
+import com.weilai.commons.log.LogOutput;
+import com.weilai.commons.log.dto.LogDto;
+
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
-	@Override
-	public void configure(WebSecurity web){
-		web.ignoring().antMatchers("/resources/**");
-	}
+@Override
+protected void configure(HttpSecurity http) throws Exception{
+	http.formLogin().loginPage("/index").permitAll();
+	http.authorizeRequests().anyRequest().authenticated();
+}
 
 }
